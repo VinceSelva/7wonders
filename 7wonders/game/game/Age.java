a3359cc579a37e5b4894a957c41f5d151da212b4
MarionPOESY <poesymarion@gmail.com>
2019-02-14 13:48:49 +0100

    Ajout de la classe player et age

---
+++ 7wonders/game/game/Age.java
@@ -0,0 +1,9 @@
+package src.main.java.game.game;
+
+import java.util.ArrayList;
+
+public class Age {
+
+    public static final int Age = 1;
+
+}
---
+++ 7wonders/game/game/Player.java
@@ -0,0 +1,8 @@
+package src.main.java.game.game;
+
+import java.util.ArrayList;
+
+public class Player {
+
+
+}
