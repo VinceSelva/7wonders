334ce1a86793a0a73d3085ec8d949b6e4669a8eb
billy <francois.martin580@gmail.com>
2019-02-26 16:21:58 +0100

    Avancement du serveur

--- 7wonders/server/src/main/java/server/Server.java
+++ 7wonders/server/src/main/java/server/Server.java
@@ -1,16 +1,20 @@
 package server;
 
+import com.corundumstudio.socketio.AckRequest;
 import com.corundumstudio.socketio.Configuration;
 import com.corundumstudio.socketio.SocketIOClient;
 import com.corundumstudio.socketio.SocketIOServer;
 import com.corundumstudio.socketio.listener.ConnectListener;
+import com.corundumstudio.socketio.listener.DataListener;
+import game.Player;
 
 import java.io.PrintStream;
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 
 public class Server {
-    SocketIOServer server;
-    final Object waitingConnection = new Object();
+    private SocketIOServer server;
+    private ArrayList<Player> players;
 
     public Server(Configuration config) {
         server = new SocketIOServer(config);
@@ -22,13 +26,54 @@ public class Server {
             public void onConnect(SocketIOClient socketIOClient) {
                 System.out.println("Server - New client connected : " + socketIOClient.getRemoteAddress());
 
-                socketIOClient.sendEvent("disconnect");
-
-                synchronized (waitingConnection) {
-                    waitingConnection.notify();
+                // 3 joueurs requis
+                if (players.size() < 3) {
+                    Player p = new Player(socketIOClient);
+                    players.add(p);
                 }
             }
         });
+
+        server.addEventListener("identification", String.class, new DataListener<String>() {
+            @Override
+            public void onData(SocketIOClient client, String data, AckRequest ackSender) throws Exception {
+                Player p = findPlayer(client);
+
+                if (p != null) {
+                    p.setname(data);
+
+                    for (Player pl: players) {
+                        pl.getSocket().sendEvent("playerConnected", p.getname());
+                    }
+                }
+            }
+        });
+    }
+
+    private Player findPlayer(SocketIOClient client) {
+        Player p = null;
+
+        for (Player pl: players) {
+            if (pl.getSocket().equals(client)) {
+                p = pl;
+                break;
+            }
+        }
+
+        return p;
+    }
+
+    private boolean allIdentified() {
+        boolean result = true;
+
+        for (Player p: players) {
+            if (p.getname() == null) {
+                result = false;
+                break;
+            }
+        }
+
+        return result;
     }
 
     private void start() {
@@ -36,16 +81,6 @@ public class Server {
 
         System.out.println("Server - Waiting for connection");
 
-        synchronized (waitingConnection) {
-            try {
-                waitingConnection.wait();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-                System.err.println("Server - Error during wait");
-            }
-        }
-
-        System.out.println("Server - New connection, stopping server.");
         server.stop();
     }
 
