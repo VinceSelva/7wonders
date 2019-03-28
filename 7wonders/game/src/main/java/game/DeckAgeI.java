3d77b5933784d468176b80f654b3cc9c492740f4
billy <francois.martin580@gmail.com>
2019-03-28 09:09:49 +0100

    Ajout de nouvelles cartes pour pouvoir distribuer 7 cartes par joueur

--- 7wonders/game/src/main/java/game/DeckAgeI.java
+++ 7wonders/game/src/main/java/game/DeckAgeI.java
@@ -12,17 +12,31 @@ public class DeckAgeI extends Deck {
 		addCard(new Card(CardType.RAW_MATERIAL, "CAVITÉ", RawMaterials.STONE));
 		addCard(new Card(CardType.RAW_MATERIAL, "BASSIN ARGILEUX", RawMaterials.BRICK));
 		addCard(new Card(CardType.RAW_MATERIAL, "FILON", RawMaterials.ORE));
-		addCard(new Card(CardType.RAW_MATERIAL, "FRICHE", RawMaterials.WOOD));
 		addCard(new Card(CardType.RAW_MATERIAL, "EXCAVATION", RawMaterials.STONE));
 		addCard(new Card(CardType.RAW_MATERIAL, "FOSSE ARGILEUSE", RawMaterials.BRICK));
 		addCard(new Card(CardType.RAW_MATERIAL, "EXPLOITATION FORESTIÈRE", RawMaterials.STONE));
-		addCard(new Card(CardType.RAW_MATERIAL, "GISEMENT", RawMaterials.WOOD));
-		addCard(new Card(CardType.RAW_MATERIAL, "MINE", RawMaterials.ORE));
+
+		// Ajout des cartes Produits Manufacturés
+		addCard(new Card(CardType.MANUFACTURED_GOOD, "MÉTIER À TISSER", ManufacturedGood.TEXTILES));
+		addCard(new Card(CardType.MANUFACTURED_GOOD, "VERRERIE", ManufacturedGood.GLASS));
+		addCard(new Card(CardType.MANUFACTURED_GOOD, "PRESSE", ManufacturedGood.PAPYRUS));
+
+		// Doublage des cartes pour avoir le bon nombre de cartes
+		addCard(new Card(CardType.RAW_MATERIAL, "CHANTIER", RawMaterials.WOOD));
+		addCard(new Card(CardType.RAW_MATERIAL, "CAVITÉ", RawMaterials.STONE));
+		addCard(new Card(CardType.RAW_MATERIAL, "BASSIN ARGILEUX", RawMaterials.BRICK));
+		addCard(new Card(CardType.RAW_MATERIAL, "FILON", RawMaterials.ORE));
+		addCard(new Card(CardType.RAW_MATERIAL, "EXCAVATION", RawMaterials.STONE));
+		addCard(new Card(CardType.RAW_MATERIAL, "FOSSE ARGILEUSE", RawMaterials.BRICK));
+		addCard(new Card(CardType.RAW_MATERIAL, "EXPLOITATION FORESTIÈRE", RawMaterials.STONE));
+		addCard(new Card(CardType.MANUFACTURED_GOOD, "MÉTIER À TISSER", ManufacturedGood.TEXTILES));
+		addCard(new Card(CardType.MANUFACTURED_GOOD, "VERRERIE", ManufacturedGood.GLASS));
+		addCard(new Card(CardType.MANUFACTURED_GOOD, "PRESSE", ManufacturedGood.PAPYRUS));
 
 		// Ajout des cartes Bâtiments Civils
 		addCard(new Card(CardType.CIVILIAN_STRUCTURE, "PRÊTEUR SUR GAGES", 3));
 		addCard(new Card(CardType.CIVILIAN_STRUCTURE, "BAINS", 3));
-		addCard(new Card(CardType.CIVILIAN_STRUCTURE, "AUTEL", 2));
+		addCard(new Card(CardType.CIVILIAN_STRUCTURE, "AUTEL", RawMaterials.BRICK, 2));
 		addCard(new Card(CardType.CIVILIAN_STRUCTURE, "THÉÂTRE", 2));
 
         	// Ajout des cartes Bâtiments Commerciaux
