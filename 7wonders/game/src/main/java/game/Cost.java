3f8b01407e65fb69903271239e1de63e2b15fd6d
Vincent Selva <vincenzo.selva@hotmail.fr>
2019-04-09 16:12:45 +0200

    Ajout de la Javadoc pour la classe Cost #36

--- 7wonders/game/src/main/java/game/Cost.java
+++ 7wonders/game/src/main/java/game/Cost.java
@@ -11,20 +11,30 @@ public class Cost {
 		this.coinsCost = 0;
 	}
 
+	/**
+	 * @param resourcesCost represente le cout de la carte pour la posée
+	 */
 	public Cost(HashMap<String, Integer> resourcesCost) {
 		this.resourcesCost = resourcesCost;
 		this.coinsCost = 0;
     }
-
+	/**
+	 * @param coins represente le cout en coins
+	 */
     public Cost(int coins) {
 		this.resourcesCost = new HashMap<>();
 		this.coinsCost = coins;
 	}
-
+	/**
+	 * @return resourcesCost retourne le cout en ressources
+	 */
 	public HashMap<String, Integer> getResourcesCost() {
 		return resourcesCost;
 	}
 
+	/**
+	 * @return coinsCost retourne le cout en coins
+	 */
 	public int getCoinsCost() {
 		return coinsCost;
 	}
