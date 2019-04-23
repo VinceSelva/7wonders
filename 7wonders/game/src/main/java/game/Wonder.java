ccfa471d57847dc07ef25962c2e4280be6dfb9be
billy <francois.martin580@gmail.com>
2019-04-23 20:13:42 +0200

    Ajout de la documentation sur la classe Wonder - ISSUE #36

--- 7wonders/game/src/main/java/game/Wonder.java
+++ 7wonders/game/src/main/java/game/Wonder.java
@@ -4,19 +4,40 @@ public class Wonder {
     private String name;
     private Enum producedResource;
 
-    public Wonder(String name, Enum producedResource) {
+    /**
+     * Modélise une merveille
+     *
+     * @param name le nom de la merveille
+     * @param producedResource la ressource produite par la merveille
+     */
+    Wonder(String name, Enum producedResource) {
         this.name = name;
         this.producedResource = producedResource;
     }
 
+    /**
+     * Récupère le nom de la merveille
+     *
+     * @return le nom de la merveille
+     */
     public String getName() {
         return name;
     }
 
-    public Enum getWonderResource() {
+    /**
+     * Récupère la ressource produite par la merveille
+     *
+     * @return la ressource produite par la merveille
+     */
+    Enum getWonderResource() {
         return producedResource;
     }
 
+    /**
+     * Retourne les informations sur la merveille
+     *
+     * @return les informations sur la merveille au format [Wonder nomDeLaMerveille, produces ressourceProduite]
+     */
     public String toString() {
         return "[Wonder " + name + ", produces " + producedResource + "]";
     }
