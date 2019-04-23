16bc165f0598e2f07ac0e9f7ee740059d65ff5a8
billy <francois.martin580@gmail.com>
2019-04-23 20:26:45 +0200

    Ajout de commentaires dans la classe ClientType - ISSUE #36

--- 7wonders/client/src/main/java/client/ClientType.java
+++ 7wonders/client/src/main/java/client/ClientType.java
@@ -1,5 +1,11 @@
 package client;
 
+/**
+ * Représente un type de client
+ *
+ * PLAY_FIRST_CARD : le client essayera de joueur tout le temps la première carte de sa main
+ * TRY_ONE_CARD : le client essayera de jouer une carte dans sa main
+ */
 public enum ClientType {
     PLAY_FIRST_CARD,
     TRY_ONE_CARD
