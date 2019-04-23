4ef83059c24fef8139147bc428bcfec3f6035472
billy <francois.martin580@gmail.com>
2019-04-23 20:27:10 +0200

    Ajout de commentaires dans la classe WonderList - ISSUE #36

--- 7wonders/game/src/main/java/game/WonderList.java
+++ 7wonders/game/src/main/java/game/WonderList.java
@@ -6,6 +6,9 @@ import java.util.Collections;
 public class WonderList {
     private ArrayList<Wonder> wonders;
 
+    /**
+     * Modélise une liste des différentes merveilles
+     */
     public WonderList() {
         wonders = new ArrayList<>();
         addWonder(new Wonder("GIZAH", RawMaterial.STONE)) ;
@@ -14,26 +17,56 @@ public class WonderList {
         addWonder(new Wonder("RHODOS", RawMaterial.ORE));
     }
 
-    public void addWonder(Wonder wonder) {
+    /**
+     * Ajoute une merveille à la liste
+     *
+     * @param wonder la merveille à ajouter
+     */
+    void addWonder(Wonder wonder) {
         wonders.add(wonder);
     }
 
+    /**
+     * Mélange la liste des merveilles
+     */
     public void shuffle() {
         Collections.shuffle(wonders);
     }
 
+    /**
+     * Récupère une merveille dans la liste
+     *
+     * @param i la position de la merveille dans la liste (0 à nombreMerveilles-1)
+     * @return la merveille récupérée (Wonder)
+     */
     public Wonder getWonder(int i) {
         return wonders.get(i);
     }
 
-    public void removeWonder(int i) {
+    /**
+     * Supprime une merveille de la liste
+     *
+     * @param i la position de la merveille dans la liste (0 à nombreMerveilles-1)
+     */
+    void removeWonder(int i) {
         wonders.remove(i);
     }
 
-    public ArrayList<Wonder> getwonders() {
+    /**
+     * Récupère la liste des merveilles
+     *
+     * @return la liste des merveilles (ArrayList de Wonder)
+     */
+    ArrayList<Wonder> getwonders() {
         return wonders;
     }
 
+    /**
+     * Récupère une merveille en fonction de son nom
+     *
+     * @param name le nom de la merveille à récupérer
+     * @return la merveille récupérée (Wonder)
+     */
     public Wonder nameToWonder(String name) {
         Wonder wonder = null;
 
